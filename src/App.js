6a4ac2923a07a01bf981de2c451097d110c45288
neeraj027 <neerajkshatriy@gmail.com>
2024-07-26 22:28:46 +0530

    index.js updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import "./App.scss";
-import {BrowserRouter as Router,Routes,Route} from  "react-router-dom";
-function App(){
-  return <Router>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-    </Routes>
-  </Router>
-    
-  
+import React from 'react';
+import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './component/Home/Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
 
-
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
